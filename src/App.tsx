08b95509b5bf9b3ee5f19bff2771200ef97387da
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-10-14 22:48:37 +0600

    add grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Dna } from 'react-loader-spinner';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Dna
-        visible={true}
-        height="180"
-        width="180"
-        ariaLabel="dna-loading"
-        wrapperStyle={{}}
-        wrapperClass="dna-wrapper d-xxl-none"
-      />
-      <Form.Control className="col-auto" placeholder="Add your item here..." />
-      <Button variant="secondary">Submit</Button>
-      <Button variant="outline-danger">Reset</Button>
+      <Stack className="container" direction="horizontal" gap={1} style={{ minHeight: '75%' }}>
+        <Collapse in={true} dimension="width" appear={true} timeout={700}>
+          <div id="example-collapse-text" className="p-2 col-2 bg-dark" style={{ minHeight: '85%', borderRadius: 3 }}>
+            Привет
+          </div>
+        </Collapse>
+        <div className="p-2 col-10 bg-dark" style={{ minHeight: '85%', borderRadius: 3 }}>
+        </div>
+      </Stack>
     </>
   );
 }
